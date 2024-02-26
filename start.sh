873e06cfd072088c1521e3f023014793ce92a76b
MichaelSchaapDev <michael_schaap@outlook.com>
2024-02-26 14:38:31 +0100

    fix: canonical url

--- start.sh
+++ start.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-# Print the current user
-whoami
-
 echo "Contact: https://www.mycleancity.nl/index.php#contacttwo" > /usr/share/nginx/html/.well-known/security.txt
-echo "Expires: 2025-10-31T23:59:01+00:00" >> /usr/share/nginx/html/.well-known/security.txt
+echo "Expires: 2024-10-31T23:59:01+00:00" >> /usr/share/nginx/html/.well-known/security.txt
 echo "Preferred-Languages: nl,en" >> /usr/share/nginx/html/.well-known/security.txt
 echo "Canonical: ${signalenCanonicalUrl}" >> /usr/share/nginx/html/.well-known/security.txt
 
