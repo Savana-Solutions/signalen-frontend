580fbdab644fc997a157d7a2358eb3467c17875d
MichaelSchaapDev <michael_schaap@outlook.com>
2025-06-26 16:25:16 +0200

    fix: stadsdelen

--- src/signals/incident-management/definitions/stadsdeelList.js
+++ src/signals/incident-management/definitions/stadsdeelList.js
@@ -1,18 +1,11 @@
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2018 - 2021 Gemeente Amsterdam
 const stadsdeelList = [
-  { key: 'berghem', value: 'Berghem' },
-  { key: 'ckm', value: 'CKM' },
-  { key: 'geffen', value: 'Geffen' },
-  { key: 'herpen', value: 'Herpen' },
-  { key: 'lith', value: 'Lith' },
-  { key: 'mhm', value: 'MHM' },
-  { key: 'nw', value: 'NW' },
-  { key: 'oijen-teeffelen', value: 'Oijen-Teeffelen' },
-  { key: 'oss-zuid', value: 'Oss-Zuid' },
-  { key: 'ravenstein', value: 'Ravenstein' },
-  { key: 'ruwaard', value: 'Ruwaard' },
-  { key: 'schadewijk', value: 'Schadewijk' },
+  { key: 'vidyadhar-nagar', value: 'Vidyadhar Nagar' },
+  { key: 'jhotwara', value: 'Jhotwara' },
+  { key: 'sanganer', value: 'Sanganer' },
+  { key: 'bagru', value: 'Bagru' },
+  { key: 'malviya-nagar', value: 'Malviya Nagar' },
   { key: 'null', value: 'Niet bepaald' },
 ]
 
